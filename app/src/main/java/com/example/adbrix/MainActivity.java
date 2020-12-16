0c912509d54e9eec2b01184cd58b5761afd17c22
gwanggyu park <pgg961@gmail.com>
2020-12-16 22:10:41 +0900

    user properties test

--- app/src/main/java/com/example/adbrix/MainActivity.java
+++ app/src/main/java/com/example/adbrix/MainActivity.java
@@ -17,6 +17,7 @@ import com.igaworks.v2.core.AdBrixRm;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button testBtn;
     TextInputEditText testEditText;
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,25 +25,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         testBtn = (Button) findViewById(R.id.btn_test);
         testEditText = (TextInputEditText) findViewById(R.id.et_test);
+        loginBtn = (Button) findViewById(R.id.btn_login);
+
+        loginBtn.setOnClickListener(this);
         testBtn.setOnClickListener(this);
         AdBrixRm.setEventUploadCountInterval(AdBrixRm.AdBrixEventUploadCountInterval.MIN);
     }
 
     @Override
     public void onClick(View v) {
-        String inputString = testEditText.getText().toString().trim();
-        if (isEmpty(inputString)) {
-            return;
-        } else {
-            AdBrixRm.AttrModel eventAttr = new AdBrixRm.AttrModel().setAttrs("search_string", inputString);
-            AdBrixRm.event("search_event", eventAttr);
-            Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
-            intent.putExtra("search_string", inputString);
-            startActivity(intent);
+
+        switch (v.getId()) {
+            case R.id.btn_test: {
+                String inputString = testEditText.getText().toString().trim();
+                if (isEmpty(inputString)) {
+                    return;
+                } else {
+                    AdBrixRm.AttrModel eventAttr = new AdBrixRm.AttrModel().setAttrs("search_string", inputString);
+                    AdBrixRm.event("search_event", eventAttr);
+                    Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+                    intent.putExtra("search_string", inputString);
+                    startActivity(intent);
+                }
+                break;
+            }
+            case R.id.btn_login: {
+                sendLoginLog();
+                Toast.makeText(this, "test", Toast.LENGTH_LONG).show();
+                break;
+            }
         }
+
+
     }
 
-    public boolean isEmpty(String cityName){
+
+    public String getUserType(){
+        return "멘티";
+    }
+
+
+
+    public void sendLoginLog() {
+
+        AdBrixRm.login("test_user_id");
+        AdBrixRm.UserProperties userProperties = new AdBrixRm.UserProperties();
+        userProperties.setAttrs("user_type", getUserType());
+        AdBrixRm.saveUserProperties(userProperties);
+    }
+
+
+    public boolean isEmpty(String cityName) {
         if (cityName.equals("") || cityName.equals(null)) {
 
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
